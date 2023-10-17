87568596a85578fe09c53563d95ca7bd7db8722e
GiselleLop <giselleLop@users.noreply.github.com>
2023-10-17 09:19:11 -0500

    orden de los archivos js

--- src/main.js
+++ src/main.js
@@ -1,49 +1,53 @@
-  import { getFirestore, collection, getDocs  } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-  import { getAuth,signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
   
+  import { renderCreateAccount } from "./register.js";
+  /*const index = 'index.html'
+
+  const routes = [
+  { path: '/', component: index},
+    {path: '/register', component: renderCreateAccount()}
+  ]
+  const defaultRoute = '/';
+  const root = document.querySelector('.homepage')
+
+
+  function navigateTo(hash) {
+    const route = routes.find((routeFound) => routeFound.path === hash);
+    
+
+    if (route && route.component) {
+      window.history.pushState(
+        {},
+        route.path,
+        window.location.origin + route.path,
+      );
   
-  import { renderCreateAccount } from "./render.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+      if (root.firstChild) {
+        root.removeChild(root.firstChild);
+      }
+      root.appendChild(route.component());
+     }
+     else {
+      navigateTo('/error');
+    }
+  }
+  
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+navigateTo(window.location.pathname || defaultRoute);
+*/
 
-    const firebaseConfig = {
-    apiKey: "AIzaSyA_nNPVRwXqmgLlxdYL4NmJwiItX9t2D5E",
-    authDomain: "social-network-c61c9.firebaseapp.com",
-    projectId: "social-network-c61c9",
-    storageBucket: "social-network-c61c9.appspot.com",
-    messagingSenderId: "496904934051",
-    appId: "1:496904934051:web:4cc5210ca6b1e3661c2516",
-    measurementId: "G-ZBR0EJZW08"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const auth = getAuth(app)
-  const firestore = getFirestore(app);
-  /*
-// Para crear o registrar usuarios
-const buttonSignin = document.querySelector('#buttonSignin');
-buttonSignin.addEventListener('click', (e) => {
-  e.preventDefault();
- const singUpEmail = document.querySelector('#emailIn').value;
- const singPassword = document.querySelector('#passwordIn').value;
- //console.log(singUpEmail , singPassword);
+  const register = document.querySelector('#registerButton') 
+register.addEventListener('click', () => {
+  renderCreateAccount();
+  console.log("render");
 
- createUserWithEmailAndPassword(auth, singUpEmail, singPassword)
-    .then(userCredential => {
-      console.log('signup');
-    })
-    .catch(error => {
-      console.error('Error al registrarse:', error.message);
-    });
 })
 
+
+  /*
 
 // Para iniciar sesion o ingresar 
 const buttonLogin =  document.querySelector('#buttonLogin');
@@ -61,54 +65,5 @@ signInWithEmailAndPassword(auth, loginEmail, loginPassword)
   console.error('Error al registrarse:', error.message);
 });
 })
-
-
-const div = document.querySelector('#close')
-const list = document.createElement("ul")
-div.appendChild(list)
-
-
-function setupPost(data) {
-if(data.length) {
-  let html = "";
-  data.forEach(doc => {
-    const post = doc.data()
-    const li = `
-    <li class="ListGroupItem">
-    <h5>${post.title}</h5>
-    <p>${post.description}</p>
-    </li>
-    `;
-    html += li;
-  });
-  list.innerHTML= html;
-} else {
-  list.innerHTML = `<p>Login to see posts</p>`
-}
-}
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // Ahora user es el objeto de usuario autenticado
-    // Puedes acceder a la colección de 'post' del usuario
-    const userPostsCollection = collection(firestore, "post");
-
-    getDocs(userPostsCollection).then((snapshot) => {
-      // snapshot.docs contiene los documentos de la colección
-      setupPost(snapshot.docs);
-      console.log(snapshot.docs);
-    });
-  } else {
-    setupPost([])
-    console.log("Sing out");
-  }
-});
-//import { myFunction } from './lib/index.js';
-
-//myFunction();
 */
-
-
-const register = document.querySelector('#registerButton') 
-register.addEventListener('click', renderCreateAccount)
 
---
+++ src/register.js
@@ -0,0 +1,111 @@
+import { auth } from "./firebase";
+import { renderPosts } from "./firebase";
+import { createUser } from "./firebase";
+import { GoogleRegister } from "./firebase";
+
+
+//para crear la vista de registro 
+export function renderCreateAccount(){
+    const main = document.querySelector('.homepage')
+    main.innerHTML= ""
+  //header
+  const header = document.createElement("header");
+  header.setAttribute("class", "headerRegister")
+  
+  //Titulo de registro
+    const title = document.createElement("h2");
+    title.textContent = "Para crear una nueva cuenta, ingresa tus datos."
+  // Imagen
+    const image = document.createElement("img")
+    image.src = '/src/evolucion.png'
+    image.style.width = '60%';
+    image.style.height = 'auto';
+  
+  //Contenedor de los inputs
+    const container = document.createElement("div")
+    container.setAttribute("class", "Inputcontainer")
+  //input Username
+    const username = document.createElement("h4")
+    username.textContent = "Nombres completos:";
+    username.setAttribute("class", "h4Register");
+    const inputName = document.createElement("input")
+    inputName.setAttribute("type", "text");
+    inputName.setAttribute("class", "input")
+  // input correo
+    const email = document.createElement("h4")
+    email.textContent = "Correo Electrónico:"
+    email.setAttribute("class", "h4Register");
+    const inputEmail = document.createElement("input")
+    inputEmail.setAttribute("type", "email");
+    inputEmail.setAttribute("class", "input")
+    inputEmail.setAttribute("id", "emailRegister")
+  //input contrasena
+    const password = document.createElement("h4")
+    password.textContent = "Contraseña:";
+    password.setAttribute("class", "h4Register");
+    const inputPassword = document.createElement("input")
+    inputPassword.setAttribute("type", "password");
+    inputPassword.setAttribute("class", "input")
+    inputPassword.setAttribute("id", "passwordRegister")
+  // boton continuar
+    const buttonContinue = document.createElement("button");
+    buttonContinue.textContent = "Continuar"
+    buttonContinue.setAttribute("class", "buttonRegister")
+    buttonContinue.setAttribute("id", "continue")
+  //google boton
+  const or = document.createElement("h4")
+  or.textContent = "o";
+  or.setAttribute("id", "or")
+
+  const buttonGoogle = document.createElement("button");
+  buttonGoogle.textContent= "Registrarse con Google"
+  buttonGoogle.setAttribute("class", "buttonRegister")
+//boton volver
+  const buttonBack = document.createElement("button");
+  buttonBack.textContent = "Volver"
+  buttonBack.setAttribute("class", "buttonRegister");
+  buttonBack.addEventListener("click", () => {
+    location.href="index.html";
+  })
+  //boton x
+  const postx = document.createElement("button");
+  postx.setAttribute("class", "buttonRegister");
+  postx.textContent = "posts"
+  postx.addEventListener("click", ()=>{
+    main.innerHTML= "";
+ 
+    //contenerdor de posts
+    const postContainer = document.createElement("div");
+    postContainer.setAttribute("class", "postContainer");
+    
+    const buttonback = document.createElement("button");
+    buttonback.textContent= "back";
+    buttonback.addEventListener("click", ()=> {
+      renderCreateAccount()
+    })
+  main.appendChild(postContainer)
+  main.appendChild(buttonback)
+  
+    renderPosts()
+  })
+
+    main.append(header, container)
+    header.append(title, image);
+    container.append(username, inputName, email, inputEmail, password, inputPassword, buttonContinue, or, buttonGoogle, buttonBack, postx)
+   
+    // continuar para registrar
+ buttonContinue.addEventListener("click", (e)=> {
+  e.preventDefault();
+  const singUpEmail = document.querySelector('#emailRegister').value;
+  const singPassword = document.querySelector('#passwordRegister').value;
+  //console.log(singUpEmail , singPassword);
+  createUser(auth, singUpEmail, singPassword);
+ })
+// Con Google
+buttonGoogle.addEventListener("click", (e) => {
+   GoogleRegister()
+
+});
+return main
+
+}
